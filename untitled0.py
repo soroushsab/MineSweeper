a18ca973379e0dd79c2086c033d7993919a2a9e5
soroushsab <soroushsabeti.s@gmail.com>
2021-11-18 10:50:35 +0000

    Start Git

---
+++ untitled0.py
@@ -0,0 +1,124 @@
+
+from tkinter import *
+
+class Mine():
+    def __init__(self,ch = ""):
+        
+        if ch != "":
+            ch.destroy()
+        self.root = Tk()
+        self.root.title('Minesweeper')
+        
+        self.w_root = 400
+        self.h_root = 400
+        
+        self.root.geometry('%dx%d+%d+%d' % (self.w_root, self.h_root, 
+                                       ((self.root.winfo_screenwidth()/2)-(self.w_root/2))
+                                       , ((self.root.winfo_screenheight()/2)-(self.h_root/2))))
+        
+        self.lbl_title = Label(self.root,text='Wellcome to Soroush made game!').place(x = 110,y = 25)
+        self.lbl_lvl_selection = Label(self.root,text='Please select level of game you want to do: ').place(x = 90,y = 75)
+        
+        self.btn_easy = Button(self.root, text = "Easy!",command=lambda: self.easy_game()).place(x = 120, y = 100)
+        self.btn_normal = Button(self.root, text = "Normal!",command=lambda: self.normal_game()).place(x = 170, y = 100)
+        self.btn_hard = Button(self.root, text = "Hard!",command=lambda: self.hard_game()).place(x = 235, y = 100)
+        
+        self.btn_close = Button(self.root, text = "Close",command=lambda: self.close_program()).place(x = self.w_root - 50, y = self.h_root - 50)
+        
+        self.root.mainloop()
+        
+    def easy_game(self):
+        self.root.destroy()
+        self.root = Tk()
+        self.root.title('Minesweeper')
+        
+        self.w_root = 300
+        self.h_root = 300
+        
+        self.root.geometry('%dx%d+%d+%d' % (self.w_root, self.h_root, 
+                                       ((self.root.winfo_screenwidth()/2)-(self.w_root/2))
+                                       , ((self.root.winfo_screenheight()/2)-(self.h_root/2))))
+        
+        self.lbl_title = Label(self.root,text='Easy!').place(x = 10,y = 10)
+    
+        self.btn_reset = Button(self.root, text = "Reset!",command=lambda: self.easy_game()).place(x = self.w_root - 50, y = 10)
+        
+        self.btn_back = Button(self.root, text = "Back!",command=lambda: self.__init__(self.root)).place(x = self.w_root - 100, y = 10)
+        
+        self.score = 0
+            
+        self.lbl_title = Label(self.root,text='Your score: '+str(self.score)).place(x = 70,y = 10)
+        
+        self.n_table = 10
+        
+        self.b = [[0]*self.n_table]*self.n_table
+        for i in range(self.n_table):
+            for j in range(self.n_table):
+                self.b[i][j] = Button(self.root, text = "#").place(x = 50 + i*20, y = 70 + j*20)
+    
+    def normal_game(self):
+        self.root.destroy()
+        self.root = Tk()
+        self.root.title('Minesweeper')
+        
+        self.w_root = 450
+        self.h_root = 450
+        
+        self.root.geometry('%dx%d+%d+%d' % (self.w_root, self.h_root, 
+                                       ((self.root.winfo_screenwidth()/2)-(self.w_root/2))
+                                       , ((self.root.winfo_screenheight()/2)-(self.h_root/2))))
+        
+        self.lbl_title = Label(self.root,text='Normal!').place(x = 10,y = 10)
+    
+        self.btn_reset = Button(self.root, text = "Reset!",command=lambda: self.normal_game()).place(x = self.w_root - 50, y = 10)
+        
+        self.btn_back = Button(self.root, text = "Back!",command=lambda: self.__init__(self.root)).place(x = self.w_root - 100, y = 10)
+        
+        self.score = 0
+            
+        self.lbl_title = Label(self.root,text='Your score: '+str(self.score)).place(x = 70,y = 10)
+        
+        self.n_table = 17
+        
+        self.b = [[0]*self.n_table]*self.n_table
+        for i in range(self.n_table):
+            for j in range(self.n_table):
+                self.b[i][j] = Button(self.root, text = "#").place(x = 50 + i*20, y = 70 + j*20)
+    
+    def hard_game(self):
+        self.root.destroy()
+        self.root = Tk()
+        self.root.title('Minesweeper')
+        
+        self.w_root = 600
+        self.h_root = 600
+        
+        self.root.geometry('%dx%d+%d+%d' % (self.w_root, self.h_root, 
+                                       ((self.root.winfo_screenwidth()/2)-(self.w_root/2))
+                                       , ((self.root.winfo_screenheight()/2)-(self.h_root/2))))
+        
+        self.lbl_title = Label(self.root,text='Hard!').place(x = 10,y = 10)
+    
+        self.btn_reset = Button(self.root, text = "Reset!",command=lambda: self.hard_game()).place(x = self.w_root - 50, y = 10)
+        
+        self.btn_back = Button(self.root, text = "Back!",command=lambda: self.__init__(self.root)).place(x = self.w_root - 100, y = 10)
+        
+        self.score = 0
+            
+        self.lbl_title = Label(self.root,text='Your score: '+str(self.score)).place(x = 70,y = 10)
+        
+        self.n_table = 25
+        
+        self.b = [[0]*self.n_table]*self.n_table
+        for i in range(self.n_table):
+            for j in range(self.n_table):
+                self.b[i][j] = Button(self.root, text = "#").place(x = 50 + i*20, y = 70 + j*20)
+    
+        
+        
+    def close_program(self):
+        self.root.destroy()
+        
+        
+
+m = Mine()
